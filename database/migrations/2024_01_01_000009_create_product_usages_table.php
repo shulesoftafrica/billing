9a4a02c86b2f630da31167a40e4831c80d1e679a
David Daniel <=dd4227112@gmail.com>
2026-01-30 12:00:01 +0300

    added new migration for usage type products

---
+++ database/migrations/2024_01_01_000009_create_product_usages_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('product_usages', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('product_id')->constrained('products')->onDelete('cascade');
+            $table->foreignId('customer_id')->constrained('customers')->onDelete('cascade');
+            $table->integer('quantity')->default(0);
+            $table->timestampsTz();
+        });
+        Schema::table('product_usages', function (Blueprint $table) {
+            $table->index('product_id');
+            $table->index('customer_id');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('product_usages');
+    }
+};
