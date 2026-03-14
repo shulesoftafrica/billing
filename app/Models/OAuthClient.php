3ce6ae4e7be2b7b0e25afed6e3fdc64af2b37cbf
Ephraim Swilla <swillae1@gmail..com>
2026-03-14 18:06:51 +0300

    Fix OAuthClient table name mapping
    - Explicitly set table name to 'oauth_clients' in OAuthClient model
    - Resolves error: relation 'o_auth_clients' does not exist
    - Laravel was incorrectly pluralizing OAuthClient to o_auth_clients

--- app/Models/OAuthClient.php
+++ app/Models/OAuthClient.php
@@ -10,6 +10,13 @@ class OAuthClient extends Model
 {
     use HasFactory;
 
+    /**
+     * The table associated with the model.
+     *
+     * @var string
+     */
+    protected $table = 'oauth_clients';
+
     protected $fillable = [
         'organization_id',
         'name',
